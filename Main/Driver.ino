14a8232f8ade43f8d789048d5a203104e6bc0bbd
cedsmit <cedsmit@gmail.com>
2022-07-08 05:16:09 +0200

    Curling Assignment

---
+++ Main/Driver.ino
@@ -0,0 +1,42 @@
+// Calculation percentage is for easier use.
+void driveForward(double percentage) {
+  int speedR = int((255.0f / 100.0f) * percentage);
+  int speedL = int((225.0f / 100.0f) * percentage);
+  
+  analogWrite(motorPinRA, speedR);
+  analogWrite(motorPinRV, 0);
+  analogWrite(motorPinLV, 0);
+  analogWrite(motorPinLA, speedL);
+}
+
+void turnLeft(double percentage) {
+  int speedR = int((255.0f / 100.0f) * percentage);
+  
+  analogWrite(motorPinRA, speedR);
+  analogWrite(motorPinRV, 0);
+  analogWrite(motorPinLV, 0);
+  analogWrite(motorPinLA, 0);
+}
+
+void turnRight(double percentage)  {
+  int speedL = int((225.0f / 100.0f) * percentage);
+  
+  analogWrite(motorPinRA, 0);
+  analogWrite(motorPinRV, 0);
+  analogWrite(motorPinLV, 0);
+  analogWrite(motorPinLA, speedL);
+}
+
+void stopVehicle() {
+  analogWrite(motorPinRA, 0);
+  analogWrite(motorPinRV, 0);
+  analogWrite(motorPinLV, 0);
+  analogWrite(motorPinLA, 0);
+}
+
+void driveBackwards(){
+  analogWrite(motorPinRA, 0);
+  analogWrite(motorPinRV, 0);
+  analogWrite(motorPinLV, 0);
+  analogWrite(motorPinLA, 0);
+}
---
+++ Main/Main.ino
@@ -0,0 +1,81 @@
+#include <Arduino.h>
+#include <analogWrite.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// Display setup
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_RESET     4
+#define SCREEN_ADDRESS 0x3C
+#define SSD1306_NO_SPLASH;
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// Motor config
+int motorPinLA = 5; //Linkerwiel achteruit
+int motorPinLV = 18; //Linkerwiel vooruit
+int motorPinRV = 16; //Rechterwiel vooruit
+int motorPinRA = 17; //Rechterwiel achteruit
+
+// IR config
+int IRPinR = 34;// IR pin rechts
+int IRPinL = 39;// IR pin links
+int pinR = 0;
+int pinL = 0;
+int colorThreshold = 2700;
+
+
+
+
+void setup(){
+  Serial.begin(9600);
+
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Infinite loop to stop the program from executing the rest of the code.
+  }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.display();
+
+  pinMode(motorPinRA, OUTPUT);
+  pinMode(motorPinRV, OUTPUT);
+  pinMode(motorPinLV, OUTPUT);
+  pinMode(motorPinLA, OUTPUT);
+}
+
+void loop(){
+  // clear display so we can send new data to it.
+  display.clearDisplay();
+
+  // reset cursor to top right corner of the screen.
+  display.setCursor(0,0);
+  display.println("PinR: " + String(analogRead(IRPinR)));
+  display.println("PinR: " + String(analogRead(IRPinL)));
+  display.display();
+  
+  //Read the IR pins
+  int pinL = analogRead(IRPinL) / colorThreshold;
+  int pinR = analogRead(IRPinR) / colorThreshold;
+
+    //if both pins reaches the threshold, stop the robot from moving.
+  if(pinL >= 1 && pinR >= 1){
+     stopVehicle();
+    
+    //if pin right reaches the threshold turn the robot left with % power
+  }else if(pinL == 0 && pinR >= 1){
+    turnLeft(50);
+
+    //if pin left reaches the threshold turn the robot left with % power
+  }else if(pinL >= 1 && pinR == 0){
+    turnRight(50);
+    
+    // continue moving forward if the threshold is not reached on either pins.
+  }else if(pinL == 0 && pinR == 0){
+    driveForward(40);
+  }
+}
